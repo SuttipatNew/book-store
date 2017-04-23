de71514f7419483953b4bb1caae3a333083aaaef
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-04-23 16:40:19 +0700

    connect with mysql using utf8 encoding

--- connect-data.php
+++ connect-data.php
@@ -1,16 +1,31 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 $servername = "localhost";
 $username = "user1";
 $password = "1q2w3e4r";
+$dbname = "test";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=test", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, "utf8");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "SELECT * FROM book";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["BookID"]. " - Title: " . $row["BookTitle"]. "<br>";
     }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var menu = ['home', 'books'];
+  $('a.mdl-navigation__link').click(function() {
+    var classes = $(this).attr('class');
+    var selected_menu = -1;
+    for(var i = 0; i < menu.length; i++) {
+      if(classes.indexOf(menu[i]) != -1) {
+        selected_menu = i;
+        break;
+      }
+    }
+    if(selected_menu != -1) {
+      $('div.page').removeClass('show');
+      $('div.page.'+menu[selected_menu]).addClass("show");
+      $('.mdl-layout__drawer').attr('aria-hidden', 'true');
+      load_data('hello');
+    }
+  });
+});
