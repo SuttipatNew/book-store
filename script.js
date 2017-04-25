bab71f3eae3fa96e390e0d1a36e8a06f3bc9a31c
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2017-04-25 10:07:14 +0700

    clean code

--- script.js
+++ script.js
@@ -13,18 +13,6 @@ $(document).ready(function() {
   if (! dialog.showModal) {
     dialogPolyfill.registerDialog(dialog);
   }
-  // showDialogButton.addEventListener('click', function() {
-  //   dialog.showModal();
-  // });
-  // dialog.querySelector('.close').addEventListener('click', function() {
-  //   dialog.close();
-  // });
-
-
-
-
-
-
 
   // console.log('start');
   $(document).on("click", 'a.mdl-navigation__link', function() {
